8e6ad8dc68b21aa6040a9cec1e15fe4428537c06
hemapriyavk <hemapriya2000mdu@gmail.com>
2025-04-08 19:28:53 +0530

    Database Updated

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,13 +1,30 @@
-import mysql from 'mysql2/promise.js'
+import mysql from 'mysql2';
+import 'dotenv/config';
 
-const db = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'todo_app'
-});
+const db = mysql.createConnection({
 
-export default db;
+  /* XAMPP */
+    // host: "localhost",
+    // user: "root", // Default MySQL username in XAMPP
+    // password: "", // Default is empty in XAMPP
+    // database: "employees",
+
+    host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT
+  });
+  
+ db.connect((err) => {
+    if (err) {
+      console.error("Database connection failed:", err);
+    } else {
+      console.log("Connected to MySQL Database ✅");
+    }
+  });
+
+  export default db;
 
 /*
 //db structure
--- backend/index.js
+++ backend/index.js
@@ -6,6 +6,7 @@ import session from 'express-session';
 import typeDefs from './graphql/schema.js';
 import resolvers from './graphql/resolvers.js';
 import db from './config/db.js';
+import 'dotenv/config';
 
 
 const app = express();
